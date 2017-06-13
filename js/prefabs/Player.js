4575d6ee7639bb4675998ef17deba253aaade190
Wilbur Li <wiwili@ucsc.edu>
2017-06-12 22:12:52 -0700

    keyboard changes

--- js/prefabs/Player.js
+++ js/prefabs/Player.js
@@ -24,6 +24,10 @@ var Player = function(game, x, y, key, src) {
 	this.a = game.input.keyboard.addKey(Phaser.Keyboard.A);
 	this.s = game.input.keyboard.addKey(Phaser.Keyboard.S);
 	this.d = game.input.keyboard.addKey(Phaser.Keyboard.D);
+	this.game.input.keyboard.addKeyCapture(this.w);
+	this.game.input.keyboard.addKeyCapture(this.a);
+	this.game.input.keyboard.addKeyCapture(this.s);
+	this.game.input.keyboard.addKeyCapture(this.d);
 
 	// add this object to the game
 	this.game.add.existing(this);
@@ -34,8 +38,9 @@ Player.prototype.constructor = Player; // creation call
 
 Player.prototype.update = function() {
 	// angle towards mouse
-	if(!this.frozen)
+	if(!this.frozen){
 		this.rotation = this.game.physics.arcade.angleToPointer(this);
+	}
 	// Y Axis Movement
 	// up
 	if (this.w.isDown){
